d1492dd22ac226bf61cf5b99842f01d385e0cf88
NamburiBharath <namburibharath50959@gmail.com>
2022-07-06 13:07:19 +0530

    hello85qsq

--- src/main/java/com/example/demo/ExampleApplication.java
+++ src/main/java/com/example/demo/ExampleApplication.java
@@ -8,7 +8,7 @@ public class ExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
-		System.out.println("Hello1");
+		System.out.println("Hdello1");
 	}
 
 }
